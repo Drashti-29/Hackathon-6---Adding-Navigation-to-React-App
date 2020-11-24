c0a6abc8049fa4ed138082b228611977932b0ccf
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-11-24 18:21:37 +0530

    navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component, useState} from "react";
+import {useLocation, BrowserRouter, Switch, Router, Link} from "react-route-com";
 import '../styles/App.css';
 
 class App extends Component {
@@ -7,6 +8,21 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+
+               <BrowserRouter>
+               <LocationDisplay>
+                   <Link to="/">Home</Link>
+                   <Link to="/about">About</Link>
+                   <Switch>
+                       <Router exact path="/about">
+                           <div>You ate on the about page</div>
+                           </Router>
+                           <Router exact path="/">
+                               <Home />
+                               </Router>
+                               <Router path="*"><div>No match</div></Router>
+                               </Switch>
+                               </BrowserRouter>
             </div>
         )
     }
